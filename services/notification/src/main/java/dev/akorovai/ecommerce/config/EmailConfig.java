f1f32988606e61b31633222b3929f2db2e5384bf
s24377 <s24377@pjwstk.edu.pl>
2024-08-12 20:40:11 +0200

    feat: create gateway service
    Took 16 minutes

---
+++ services/notification/src/main/java/dev/akorovai/ecommerce/config/EmailConfig.java
@@ -0,0 +1,15 @@
+package dev.akorovai.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class EmailConfig {
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+		return new JavaMailSenderImpl();
+	}
+
+}
